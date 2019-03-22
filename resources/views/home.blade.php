7da2a1d78c6a2720adb6f29b5b01fdb8196e2deb
Jon <jprather.dev@gmail.com>
2019-03-22 15:44:54 -0700

    started nav bar structure and css. Still need to do icon and registration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,21 +9,32 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         {{-- <link rel="stylesheet" href="css/app.css">\ --}}
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+       
+        
         {{-- <script src="{{ mix('/js/app.js') }}"></script> --}}
 
     </head>
     <body>
+        <div class="brand">ill float this left and add the icon in here with css class to target it</div>
        <div class = "navigation"> 
            <ul class="navigation__list"> 
            <li class="navigation__item"> <a class="navigation__link" > Home </a> </li>
-           <li class="navigation__item"> <a class="navigation__link" > Investor </a> </li>
-           <li class="navigation__item"> <a class="navigation__link" > Borrower </a> </li>
+           <li class="navigation__item"> <a class="navigation__link" > Investor <i class="fa fa-chevron-down"></i>
+           </a> </li>
+           <li class="navigation__item"> <a class="navigation__link" > Borrower <i class="fa fa-chevron-down"></i> </a> </li>
            <li class="navigation__item"> <a class="navigation__link" > Company </a> </li>
            
            </ul>
           
+       </div>
+       {{-- not sure if this is exactly the BEM structure i want to use here but gtg --}}
+       <div class="registration">
+           <div class="registration__login">
+              <a>login </a> </div>  
+           <div class="registration__sign-up">here will be a button and a link to another page ; can float right if needed or can put all of this into the navigation and make it a reusable blade layout</div>  
        </div>
     </body>
 </html>
