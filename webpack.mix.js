92bf8523532a44acef0a8ef83bdcd58356a7ee08
Jon <jprather.dev@gmail.com>
2019-03-25 23:42:52 -0700

    changes to mix-manifest and app.css would be overwritten by checkout or merge

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
-   .version()
    .browserSync('http://localhost:8888/');
+
+   //.version()  this throughs off merges bc diff in master now 
